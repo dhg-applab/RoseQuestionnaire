4550990eee67ad2638fa99eff3f4c1c39e54407e
Florian Schweizer <florian.schweizer@tum.de>
2024-05-14 13:50:35 +0200

    Add question variables

--- Rose-Questionnaire/ContentView.swift
+++ Rose-Questionnaire/ContentView.swift
@@ -9,6 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     
+    // Hinweis: Diese `Variablen` sind für die Antworten auf vier Fragen.
+    // Sie sind erst bei Teil 2 relevant.
+    @State var question1 = ""
+    @State var question2 = ""
+    @State var question3 = ""
+    @State var question4 = ""
+    
     var body: some View {
         NavigationStack {
             VStack {
